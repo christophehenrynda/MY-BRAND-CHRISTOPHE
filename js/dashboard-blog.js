34a86b8bcc5b059ecb47c1b46a8470428f63f4ee
Tophe <89867523+christophehenrynda@users.noreply.github.com>
2024-02-19 01:15:39 +0200

    Add files via upload

--- js/dashboard-blog.js
+++ js/dashboard-blog.js
@@ -6,3 +6,47 @@ function toggle(){
     let popup = document.querySelector('.add-blog');
     popup.classList.toggle('active');
 }
+function update(){
+    let header = document.querySelector('header');
+    header.classList.toggle('active');
+    let main = document.querySelector('main');
+    main.classList.toggle('active');
+    let popUp = document.querySelector('.update-blog-popup');
+    popUp.classList.toggle('active');
+}
+const dropArea = document.getElementById("update-drop-area");
+const inputFile = document.getElementById("update-input-file");
+const imageView = document.querySelector(".update-image-view");
+inputFile.addEventListener("change", uploadImage);
+function uploadImage () {
+    let imgLink = URL.createObjectURL(inputFile.files[0]);
+    imageView.style.backgroundImage = `url(${imgLink})`;
+    imageView.textContent = "";
+    imageView.style.border = 0;
+}
+dropArea.addEventListener("dragover", (e) => {
+    e.preventDefault();
+});
+dropArea.addEventListener("drop", (e) => {
+    e.preventDefault();
+    inputFile.files = e.dataTransfer.files;
+    uploadImage();
+});
+const addDropArea = document.getElementById("drop-area");
+    const addInputFile = document.getElementById("input-file");
+    const addImageView = document.querySelector(".image-view");
+    addInputFile.addEventListener("change", addBlog);
+    function addBlog () {
+        let imgLink = URL.createObjectURL(addInputFile.files[0]);
+        addImageView.style.backgroundImage = `url(${imgLink})`;
+        addImageView.textContent = "";
+        addImageView.style.border = 0;
+}
+addDropArea.addEventListener("dragover", (e) => {
+    e.preventDefault();
+});
+addDropArea.addEventListener("drop", (e) => {
+    e.preventDefault();
+    addInputFile.files = e.dataTransfer.files;
+    addBlog();
+});
--- js/login-signup.js
+++ js/login-signup.js
@@ -54,8 +54,16 @@ class Login {
             });
             if(error == 0){
                 //API
-                localStorage.setItem("auth",1)
-                this.form.submit();
+                if (this.check()) {
+                    localStorage.setItem("auth",1);
+                    this.form.submit();
+                }else {
+                    x.reset();  
+                    y.reset();
+                }
+                
+                
+                
             } 
         });
     }
@@ -98,10 +106,173 @@ class Login {
         parent.classList.add("error");
         parent.classList.remove("success");
     }
+    check ()  {
+        //stored credentials
+        let user = JSON.parse(localStorage.getItem("user")) || [];
+        
+        for (let i = 0; i < user.length; i++) {
+            const element = user[i];
+            //login credentials
+            if (element.email === loginEmail.value && element.password === loginPassword.value)  {
+                //window.location.href = "./dashboard.htm";
+                x.reset();
+                y.reset();
+                return true;
+            } 
+        }
+        return false;
+    }
+    
+        
 }
+
 
 const form = x;
 if (form){
     const fields = [loginEmail, loginPassword];
     const validator = new Login(form, fields);
 }
+
+class SignUp {
+    constructor(form,fields){
+        this.form = form;
+        this.fields = fields;
+        this.validateOnSubmit();
+    }
+
+    validateOnSubmit (){
+        let self = this;
+
+        this.form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            var err = 0;
+            self.fields.forEach((field) => {
+                const input = field;
+                if (!self.validateFields(input)) {
+                    err++;
+                } 
+            });
+            if (err == 0) {
+                //API
+                this.store();
+                window.location.href = window.location.href;
+                
+            }
+        });
+    }
+
+    validateFields(field){
+        if (field.value.trim() == '') {
+            this.onError(field, "This field is required");
+            return false;
+        }else if (field.type == "email") {
+            if (!emailRegex.test(field.value)) {
+                this.onError(field, "Enter a valid email address");
+                return false; 
+            }else {
+                this.onSuccess(field);
+                return true;
+            }
+        }else if (field.type == "password") {
+            if (!passwordRegex.test(field.value)) {
+                this.onError(field, "Please enter a valid password");
+                return false
+            }else if (!(registerPassword.value.trim() == registerPasswordConfirm.value.trim())) {
+                this.onError(registerPasswordConfirm, "Please confirm your password");
+            }
+            else {
+                this.onSuccess(field);
+                return true;
+            }
+        }else if (field.type == "text") {
+            if (field.value.length < 4){
+                this.onError(field, "Username must be atleast 4 characters long");
+                return false;
+            }else {
+                this.onSuccess(field);
+                return true;
+            }
+        }
+        
+    }
+    onSuccess (input){
+        let parent = input.parentElement;
+        let err = parent.querySelector("small");
+        err.style.display = "none";
+        err.innerText = "";
+        parent.classList.add("success");
+        parent.classList.remove("error");
+    }
+    onError (input, message){
+        let parent = input.parentElement;
+        let err = parent.querySelector("small");
+        err.style.display = "block";
+        err.innerText = message;
+        parent.classList.add("error");
+        parent.classList.remove("success");
+    }
+    
+    store () {
+        let user = {
+            username: uName.value,
+            email: signUpEmail.value,
+            password: registerPassword.value,
+
+        }
+        let usersArray = [];
+        if (localStorage.getItem('user')){
+            let arr = JSON.parse(localStorage.getItem('user')) || [];
+            for (let i=0; i< arr.length; i++) {
+            let user = {
+                username: arr[i].username,
+                email: arr[i].email,
+                password: arr[i].password
+            }
+            }
+            
+            usersArray.push(user);
+        }
+       
+        usersArray.push(user);
+
+        localStorage.setItem("user", JSON.stringify(usersArray));
+        x.reset();
+        y.reset();
+    }
+
+}
+const registerationForm = y; 
+if (registerationForm){
+    const fields = [uName, signUpEmail, registerPassword,registerPasswordConfirm]
+    const validator = new SignUp(registerationForm, fields);
+}
+
+
+
+/*let store = () => {
+    localStorage.setItem("uname", uName.value);
+    localStorage.setItem("email", signUpEmail.value);
+    if (registerPassword.value === registerPasswordConfirm.value) {
+        localStorage.setItem("password", registerPassword.value);
+        x.reset();
+        y.reset();
+
+    } else {
+        window.alert("Please enter a password again")
+    }
+}
+let check = () => {
+    //stored credentials
+    let storeName = localStorage.getItem("uname");
+    let storedEmail = localStorage.getItem("email");
+    let storedPassword = localStorage.getItem("password");
+
+    //login credentials
+    if (storedEmail === loginEmail.value && storedPassword === loginPassword.value)  {
+        window.location.href = "./dashboard.htm";
+        x.reset();
+        y.reset();
+    } else {
+        window.alert("Please enter correct email and password")
+    }
+}*/
